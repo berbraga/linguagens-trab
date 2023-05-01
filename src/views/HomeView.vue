c2cfd007f27b6a8be04d40f47e7b95003fc53570
berbraga <bernardo.sbraga@yahoo.com>
2023-05-01 16:51:23 -0300

    bernardo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <input id="input" placeholder="digite o codigo aqui: " class="Input">
+    <div class="boxButton">
+      <button @click=" this.bernardo() ">Limpa</button>
+      <button @click=" this.bernardo() ">Analizar</button>
+    </div>
+    <textarea name="results" id="results" cols="50" rows="10"></textarea>
   </div>
 </template>
 
@@ -13,6 +18,80 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      VALID: 'sentença válida:',
+      aritimetic:  'operador aritmético',
+      sentencaInvalida: 'ERRO => sentença inválida',
+      simbuloInvalido: 'ERRO => símbolo(s) inválido(s)',
+      aritimeticOperators: ['+','-','/','*'],
+      endSentence: ['$','+','-','/','*'],
+
+    };
+  },
+  methods:{
+    bernardo:function(){
+      console.clear();
+      const token = document.getElementById('input').value
+      const results = document.getElementById('results')
+      
+
+      
+      let retornos = [];
+      let sentencas = ''
+      for (let i = 0; i < token.length; i++) {
+
+        if(!this.endSentence.includes(token.charAt(i))){
+          const char = token[i];
+          console.log('no if',i, char,)
+          sentencas +=  char;
+
+        }else{
+          console.log(sentencas)
+          retornos.push(sentencas);
+          sentencas = ''; 
+          console.log(token[i])
+        
+        }
+        
+      }
+      console.log(retornos)
+
+
+
+
+
+
+
+
+      // results.value += token
+    }
   }
+
 }
 </script>
+
+<style scoped>
+
+.container{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+}
+
+.boxButton{
+
+}
+.Input{
+  width: 400px;
+  height: 50px;
+  border-radius: 20px;
+  border: 2px solid black;
+  font: black ;
+}
+
+</style>
