cfb337b686221efbcbe9b4c51002dbc3309c265c
berbraga <bernardo.sbraga@yahoo.com>
2023-05-01 18:22:08 -0300

    salving

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <input id="input" placeholder="digite o codigo aqui: " class="Input">
     <div class="boxButton">
-      <button @click=" this.bernardo() ">Limpa</button>
-      <button @click=" this.bernardo() ">Analizar</button>
+      <button @click=" this.clear() ">Limpar</button>
+      <button @click=" this.main() ">Analizar</button>
     </div>
     <textarea name="results" id="results" cols="50" rows="10"></textarea>
   </div>
@@ -21,53 +20,66 @@ export default {
   },
   data(){
     return{
+      alfabeto: ['a','b','c','d','e'],
       VALID: 'sentença válida:',
       aritimetic:  'operador aritmético',
       sentencaInvalida: 'ERRO => sentença inválida',
       simbuloInvalido: 'ERRO => símbolo(s) inválido(s)',
       aritimeticOperators: ['+','-','/','*'],
       endSentence: ['$','+','-','/','*'],
+      tabelaT:[
+        [ 11, 11,  6,  9, 11, 11],
+        [ 11, 11, 11,  9, 11, 11 ],
+        [  3, 11,  0,  7, 11, 11 ],
+        [ 11,  4, 11, 11, 11, 11 ],
+        [  5, 11, 11, 11, 11, 11 ],
+        [ 11,  2, 11, 11, 11, 11 ],
+        [ 11, 11,  0,  7, 11, 11 ],
+        [ 11, 11, 11, 11,  1, 11 ],
+        [ 11, 11, 11,  7, 11, 11 ],
+        [ 11, 11, 11, 11,  8, 11 ],
+        [ 11, 11, 11, 11, 11, 11 ],
+      ],
+      EF: [1,1,0,0,0,0,0,0,0,0,0],
+
 
     };
   },
   methods:{
-    bernardo:function(){
-      console.clear();
-      const token = document.getElementById('input').value
-      const results = document.getElementById('results')
-      
+    getSentencas: function (token){
 
-      
-      let retornos = [];
-      let sentencas = ''
+      let sentencas = [];
+      let sentenca = '';
       for (let i = 0; i < token.length; i++) {
-
         if(!this.endSentence.includes(token.charAt(i))){
-          const char = token[i];
-          console.log('no if',i, char,)
-          sentencas +=  char;
-
+          sentenca +=  token[i];
         }else{
-          console.log(sentencas)
-          retornos.push(sentencas);
-          sentencas = ''; 
-          console.log(token[i])
-        
+          sentenca+= '$';
+          sentencas.push(sentenca);
+          sentenca = ''; 
         }
-        
       }
-      console.log(retornos)
+      return sentencas;
+    },
+    main:function(){
+      console.clear();
+      const input = document.getElementById('input');
+      const results = document.getElementById('results');
+      let toke = input.value;
+      
+      const token = toke + "$";
+      const sentencas = this.getSentencas(token);
+      console.log(sentencas);
+    
+      // console.log(retornos)
 
-
-
-
-
-
-
-
-      // results.value += token
-    }
-  }
+      results.value += token
+    },
+    clear:function(){
+      document.getElementById('input').value = '';
+      document.getElementById('results').value = '';
+    },
+  },
 
 }
 </script>
